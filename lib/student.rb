4bc041f61e57d412a92cb305ccc84d6a601743e2
Irene Njeru <irenenjeru8@gmail.com>
2022-06-15 10:34:08 +0300

    task completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative "./user"
+
+class Student < User
+
+    attr_reader :knowledge
+    attr_reader :learn
+
+    def initialize 
+        @knowledge = []
+    end
+
+    def learn(stri)
+    @knowledge << stri
+    end
 
 end
